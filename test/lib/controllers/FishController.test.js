7bd436c49c04ae1d633f7fd94c394deabc7e7920
LuisAngelFnz <lfernandez@gmail.com>
2022-05-15 15:22:18 -0500

    Arreglando prueba unitaria de FishService

--- test/lib/controllers/FishController.test.js
+++ test/lib/controllers/FishController.test.js
@@ -3,7 +3,6 @@ const FishController = require('../../../lib/controllers/FishController');
 describe('Suit test for class FishController', () => {
     test('1.- Get all fish', () => {
         const result = FishController.getAllFish();
-        console.log(result);
         expect(result).not.toBeUndefined();
     });
 });
