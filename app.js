7a5b13b0b018b370a2bf2205160239e76799e907
LuisAngelFnz <lfernandez@gmail.com>
2022-05-15 18:30:42 -0500

    Metiendo try catch al envio de mensajes

--- app.js
+++ app.js
@@ -16,25 +16,32 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.on("message", (msg) => {
     const chat_id = msg.chat.id;
-    
-    const message = msg.text.trim().toLowerCase();
-    if(['/start','help'].includes(message)){
-        response = [
-            'Hola aqui puedes consultar los peces el peligro de extinción',
-            'Opciones:',
-            '* All ó Todos (Obtiene todos los peces)',
-            '* Names ó Nombres (Consulta los nombres)',
-            '* puedes mandar algún nombre para consultar su info'
-        ].join('\n')
+    try{
+        
+        const message = msg.text.trim().toLowerCase();
+        if(['/start','help'].includes(message)){
+            response = [
+                'Hola aqui puedes consultar los peces el peligro de extinción',
+                'Opciones:',
+                '* All ó Todos (Obtiene todos los peces)',
+                '* Names ó Nombres (Consulta los nombres)',
+                '* puedes mandar algún nombre para consultar su info',
+                '\nTambien puedes escribir "help"'
+            ].join('\n')
 
-    }else if(['todos', 'all'].includes(message)){
-        response = FishController.getAllFish();
-    
-    }else if(['nombres', 'names'].includes(message)){
-        response = FishController.getAllFishNames();
-    }else{
-        response = FishController.getFishByName(message);
+        }else if(['todos', 'all'].includes(message)){
+            response = FishController.getAllFish();
+        
+        }else if(['nombres', 'names'].includes(message)){
+            response = FishController.getAllFishNames();
+        }else{
+            response = FishController.getFishByName(message);
+        }
+        
+        bot.sendMessage(chat_id, response);
+
+    }catch(error){
+        bot.sendMessage(chat_id, 'Servicio temporalmente deshabilitado');        
+        console.log('Controlle error', error);
     }
-    
-    bot.sendMessage(chat_id, response);
 });
