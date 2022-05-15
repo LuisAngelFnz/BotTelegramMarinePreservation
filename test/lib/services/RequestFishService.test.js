b2a2ebe257118c57f63a2ee071826d08522e22eb
LuisAngelFnz <lfernandez@gmail.com>
2022-05-15 14:59:17 -0500

    Quitando pruebas de RequestFishService

--- test/lib/services/RequestFishService.test.js
+++ test/lib/services/RequestFishService.test.js
@@ -2,12 +2,12 @@ const RequestFishService = require('../../../lib/services/RequestFishService');
 
 describe('Test suit for clas RequestFishService', () => {
     test('1.- Get all fish', async () => {
-        const [success,all_fish] = await RequestFishService.getAllFish();
+        // const [success,all_fish] = await RequestFishService.getAllFish();
         
-        expect(success).toBe(true);
-        console.log(all_fish);
+        // expect(success).toBe(true);
+        // console.log(all_fish);
 
-        expect(all_fish).not.toBeUndefined();
-        expect(all_fish.length >= 0).toBe(true);
+        // expect(all_fish).not.toBeUndefined();
+        // expect(all_fish.length >= 0).toBe(true);
     });
 });
